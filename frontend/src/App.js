dcf5e44a0cd99424d3cfe4a1b59d4281aee617ff
LucasP <lukas.plocieniak@gmail.com>
2022-03-26 18:37:30 +0100

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>amazon-react</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
