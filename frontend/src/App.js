02356eee870ef405db1b8c36f2cf73a4912e7e9d
LucasP <lukas.plocieniak@gmail.com>
2022-03-27 19:18:05 +0200

    added react-bootstrap package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to='/'>amazon-react</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/product/:slug' element={<ProductPage />}></Route>
-        </Routes>
-      </main>
+      <div className='d-flex flex-column site-container'>
+        {' '}
+        <header>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>amazon-react</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomePage />}></Route>
+              <Route path='/product/:slug' element={<ProductPage />}></Route>
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className='text-center'>All rights reserved.</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
